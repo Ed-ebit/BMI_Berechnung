2cbee00e141d64d1240428112c8580633ac3ae8d
Lutz Boettcher <dev@iad.de>
2021-06-22 13:57:37 +0200

    For Schleife mitz Ziel Abbruch MaxBmi

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("BMI Rechner V 1.0");
         System.out.print("Bitte Ihren Namen : ");
         Scanner scanner = new Scanner(System.in) ;
         String name = scanner.nextLine();
-
         System.out.print("\nBitte Ihr Gewicht in [KG] : ");
-        int gewicht =  Integer.parseInt(scanner.nextLine());
+        float gewicht =  Float.parseFloat(scanner.nextLine());
         //int gewicht=  scanner.nextInt();
         System.out.print("\nBitte Ihre Groesse in [m] : ");
         float groesse = (float)Double.parseDouble( scanner.nextLine());
-        double bmi = gewicht/ (groesse * groesse);
+        System.out.print("\nBitte Abbruch BMi : ");
+        float maxBmi = (float)Double.parseDouble( scanner.nextLine());
+        System.out.print("\nBitte Schrittweite Gewicht in [Kg] (Default = 1Kg) : ");
+        float schrittGewicht=1.0f;
+        String eingabe="";
+        eingabe = scanner.nextLine();
+        if(! eingabe.isEmpty()){
+            schrittGewicht = Float.parseFloat(eingabe);
+        }
+        double bmi=0;
+        for (float aktGewicht=gewicht ; // Anfangswert nur Einmal!!
+              bmi <= maxBmi  ;  // Laufbedingung bei jeden Durchlauf
+             aktGewicht= aktGewicht + schrittGewicht // Increment bei jedem Durchlauf
+             )
+        {
+             bmi = aktGewicht/ (groesse * groesse);
+            String ergebnis="Normalgewicht";
 
-         short ergebnis= 1;//(short) (bmi<19.0 ?1 : bmi>25 ?3 : 2);
-         switch (ergebnis){
-             case 1 :
-                 System.out.println("Untergewicht");break;
-             case 2:
-                 System.out.println("Normalgewicht"); break;
-             case 3 :
-                 System.out.println("Übergewicht"); break;
-             default:
-                 System.out.println("Keine Ahnung ;-)"); break;
-         }
+            switch ((short) (bmi<19.0 ?1 : bmi>25 ?3 : 2)){
+                case 1 :
+                   ergebnis= "Untergewicht";break;
+                case 2: break;
+                case 3 :
+                    ergebnis="Übergewicht"; break;
+                default:
+                    ergebnis="Keine Ahnung ;-)"; break;
+            }
+            System.out.printf("%3.2f\t%2.2f\t%s\n",aktGewicht,bmi,ergebnis);
 
-
-        // Ausgabe
-        System.out.printf("Ihr BMI betraegt %.2f",  bmi);
+        }
 
     }
 }
