43219e7ff91ca4dafb43482f89a237451f293b88
Lutz Boettcher <dev@iad.de>
2021-06-24 12:42:41 +0200

    Refactoring mit statischen Methoden Teil1

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,54 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in) ;
+    static double[] bmiWerte=new double[1000];
+    static  float aktGewicht=0;
+    static float gewicht=0;
+    static float groesse=0;
+    static float minBmi=0;
+    static float schrittGewicht=.5f;
+    static int anzWoche=0;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in) ;
-        double[] bmiWerte=new double[1000];
-        float aktGewicht=0;
-        float gewicht=0;
-        float groesse=0;
-        float minBmi=0;
-        float schrittGewicht=.5f;
-        int anzWoche=0;
+        eingabeBmiWerte();
+        berechnungUndSpeicherungBmiWerte();
+        ausgabeBmiWerte();
+      }
 
+    private static void ausgabeBmiWerte() {
+        System.out.println("Der WunschBMi wird nach "+ (anzWoche -1)+" Wochen erreicht");
 
+        for (int i=0 ; i < bmiWerte.length ;i++){
+            if(bmiWerte[i] != 0.0) {
+                System.out.printf("%-10s\t %5.2f \n", i==0?"Start": "Woche "+i, bmiWerte[i]);
+            }
+        }
+    }
+
+    private static void berechnungUndSpeicherungBmiWerte() {
+        double  bmi=0;
+        do {
+            bmi = aktGewicht/ (groesse * groesse);
+            String ergebnis=
+                    bmi<19.0 ?
+                            "Untergewicht" :
+                            bmi>25 ?
+                                    "Übergewicht" :
+                                    "Normalgewicht";
+            System.out.printf("%5s\t%3.2f\t%2.2f\t%s\n",
+                    anzWoche== 0?" ":anzWoche,
+                    aktGewicht,
+                    bmi,ergebnis);
+            aktGewicht -=  schrittGewicht;
+            //++anzWoche; // anzWoche= anzWoche + 1 oder anzWoche += 1
+            bmiWerte[anzWoche++]=bmi;
+        }while(bmi >= minBmi);
+    }
+
+    private static void eingabeBmiWerte() {
         System.out.println("BMI Rechner V 1.0");
         System.out.print("Bitte Ihren Namen : ");
-
         String name = scanner.nextLine();
         System.out.print("\nBitte Ihr Gewicht in [KG] : ");
         gewicht =  Float.parseFloat(scanner.nextLine());
@@ -26,39 +60,12 @@ public class Main {
         minBmi = (float)Double.parseDouble( scanner.nextLine());
         System.out.print("\nBitte Abnahme pro Woche Gewicht in [Kg] (Default = 0.5Kg) : ");
 
-
         String eingabe="";
         eingabe = scanner.nextLine();
         if(! eingabe.isEmpty()){
             schrittGewicht = Float.parseFloat(eingabe);
         }
         aktGewicht=gewicht ;
-
         scanner.close();
-        double  bmi=0;
-       do {
-            bmi = aktGewicht/ (groesse * groesse);
-            String ergebnis=
-                    bmi<19.0 ?
-                        "Untergewicht" :
-                    bmi>25 ?
-                         "Übergewicht" :
-                         "Normalgewicht";
-            System.out.printf("%5s\t%3.2f\t%2.2f\t%s\n",
-                    anzWoche== 0?" ":anzWoche,
-                    aktGewicht,
-                    bmi,ergebnis);
-            aktGewicht -=  schrittGewicht;
-            //++anzWoche; // anzWoche= anzWoche + 1 oder anzWoche += 1
-           bmiWerte[anzWoche++]=bmi;
-        }while(bmi >= minBmi);
-
-        System.out.println("Der WunschBMi wird nach "+ (anzWoche -1)+" Wochen erreicht");
-
-        for (int i=0 ; i < bmiWerte.length ;i++){
-            if(bmiWerte[i] != 0.0) {
-                System.out.printf("%-10s\t %5.2f \n", i==0?"Start": "Woche "+i, bmiWerte[i]);
-            }
-        }
     }
 }
